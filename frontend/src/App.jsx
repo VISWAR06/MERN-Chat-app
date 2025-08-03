20dc9fc3398094cea35a16783ed3e226b32ca8ed
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-03 20:31:13 +0530

    route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-200'>
+    <div className='text-red-500'>
       hello there
     </div>
   )
 }
 
 export default App
+ 
