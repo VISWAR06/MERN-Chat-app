3e9d9e79aee70b77de1c29d6f8b4723728bcfd88
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-03 20:51:20 +0530

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex justify-between items-center mx-8 p-10'>
       
+        <h1 className='text-3xl'>BLOOOG</h1>
+      <button className='font-bold border border-yellow-500'>Login</button>
     </div>
   )
 }
