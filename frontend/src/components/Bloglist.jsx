5f76ef076fc1a5bd386230fe0299db0374fc3e49
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-03 21:36:13 +0530

    blog

--- frontend/src/components/Bloglist.jsx
+++ frontend/src/components/Bloglist.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 const Bloglist = () => {
   return (
     <div>
-        <div>
-            <ul>
-                <li>All</li>
-                <li>Tech</li>
-                <li>Artfi</li>
-                <li>Natio</li>
+        <div className='flex justify-center'>
+            <ul >
+                <button className='flex gap-32 p-5 m-5  '>
+                <li className='bg-blue-400 w-24 rounded-xl'>All</li>
+                <li className='bg-blue-400 w-24 rounded-xl'>Tech</li>
+                <li className='bg-blue-400 w-24 rounded-xl'>Artfi</li>
+                <li className='bg-blue-400 w-24 rounded-xl'>Natio</li>
+                </button>
+              
             </ul>
         </div>
       
