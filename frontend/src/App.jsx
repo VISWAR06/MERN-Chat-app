7032e9a82534abfeda77b34c13105439c3d19805
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-03 20:34:36 +0530

    route com

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
-
+import { Route,Routes } from 'react-router-dom'
 const App = () => {
   return (
     <div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,8 @@ import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <BrowserRouter>  <App /></BrowserRouter>
   
-  </StrictMode>,
+  ,
 )
