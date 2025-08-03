075fdfac3e8366ae002193f414384420023c4ddc
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-03 20:55:19 +0530

    navbar com

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const navigate=useNavigate()
   return (
     <div className='flex justify-between items-center mx-8 p-10'>
       
-        <h1 className='text-3xl'>BLOOOG</h1>
-      <button className='font-bold border-2 border-yellow-500 rounded-xl p-2 bg-blue-200'>Login</button>
+        <h1 onClick={()=>navigate('/')} className='text-3xl cursor-pointer'>BLOOOG</h1>
+      <button onClick= {()=>navigate('/admin')}className='cursor-pointer font-bold border-2 border-yellow-500 rounded-xl p-2 bg-blue-200'>Login</button>
     </div>
   )
 }
