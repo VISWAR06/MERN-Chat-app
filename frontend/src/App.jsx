ac36a5e8e15918c70ff0be8edf5342864d286033
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-03 20:32:43 +0530

    route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      hello there
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}  />
+        <Route path='/blog' element={<Blog/>}  />
+      </Routes>
+      
     </div>
   )
 }
