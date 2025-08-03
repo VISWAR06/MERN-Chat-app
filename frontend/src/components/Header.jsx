08c70803f9ae04e0d4be1135904dc28ce0b496f1
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-03 21:09:51 +0530

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-      header
+    <div className="">
+      <div className="flex justify-center items-center mt-20">
+        <p className="text-3xl rounded-xl bg-gradient-to-tr from-white to-blue-400 
+                      border-4 border-black px-6 py-3 shadow-lg">
+          Integrated with AI
+        </p>
+      </div>
+      <div className="flex justify-center mt-24 text-3xl">
+        <h1>WRITE YOUR <span className="font-bold text-4xl text-blue-400">BLOG</span> IN <br/>
+            <span className="flex justify-center">THIS PLATFORM</span>
+        </h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
