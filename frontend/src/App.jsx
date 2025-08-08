708cb6c00bb86d5711ca96feb14be9c96e0fe902
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-08 13:47:50 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import{Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Profile from './pages/Profile'
+
 const App = () => {
   return (
-  <div >
-<Routes>
-        <Route path='/' element={<Home/>}  />
-        <Route path='/login' element={<Login/>}  />
-        <Route path='/profile' element={<Profile/>}  />
-      </Routes>
+    <div>
       
     </div>
   )
