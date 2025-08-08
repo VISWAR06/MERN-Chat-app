cf7ddd36356ebb82a78531ded221e58dcd09630f
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-08 15:43:05 +0530

    api working

--- backend/Controllers/authcr.js
+++ backend/Controllers/authcr.js
@@ -1,11 +1,19 @@
 import usermodel from "../Models/usermodel.js"
 import bcrypt from 'bcryptjs'
-const login=(req,res)=>{
+const signup=async(req,res)=>{
 const{name,email,password}=req.body
 try{
+  if(password.length<8)res.status(400).send({message:"Minimum 8 length"})
+  const user= await usermodel.findOne({email})
+if(user)res.status(400).send({message:"user alrdy exits"})
+  const hashedpasswrod= await bcrypt.hash(password,10);
+const newuser=new usermodel({
+  name,email,password:hashedpasswrod
+})
+res.status(200).send({message:"user created",hashedpasswrod})
 
 }catch(e){
-
+res.status(500).send({messsage:e.message})
 }
    
 
@@ -13,7 +21,7 @@ try{
 const logout=(req,res)=>{
   res.send("logout")
 }
-const signup=(req,res)=>{
+const login=(req,res)=>{
   res.send("singup")
 }
 export  {login,logout,signup}
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import auth from './Routes/auth.route.js'
 const app=express()
 dontenv.config()
 connectdb()
+app.use(express.json())
+app.get('/all',(req,res)=>res.send("hello"))
 const port=process.env.PORT||5001
 app.use('/api/auth',auth)
 app.listen(port,()=>{
