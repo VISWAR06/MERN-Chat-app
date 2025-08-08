044c2b64e2ab4a79e484d7739559005fc55a5ac4
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-08 14:33:34 +0530

    db connect

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const app=express()
 import auth from './Routes/auth.route.js'
-
+dontenv.config()
+const port=process.env.PORT||5001
 app.use('/api/auth',auth)
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log("working")
 })
