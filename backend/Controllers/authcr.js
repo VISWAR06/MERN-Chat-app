c4f2fd81e00cb95299abc15073856763c1997df5
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-10 11:03:56 +0530

    all done

--- backend/Controllers/authcr.js
+++ backend/Controllers/authcr.js
@@ -1,29 +1,39 @@
 import usermodel from "../Models/usermodel.js"
 import bcrypt from 'bcryptjs'
+import generatetoken from "../lib/jwt.js"
 const signup=async(req,res)=>{
 const{name,email,password}=req.body
 try{
-  if(password.length<8)res.status(400).send({message:"Minimum 8 length"})
+  if(!name||!email||!password){
+    res.status(400).json({message:"Fill all the fields"})
+  }
+  if(password.length<8)res.status(400).json({message:"Minimum 8 length"})
   const user= await usermodel.findOne({email})
-if(user)res.status(400).send({message:"user alrdy exits"})
+if(user)res.status(400).json({message:"user alrdy exits"})
   const hashedpasswrod= await bcrypt.hash(password,10);
 const newuser=new usermodel({
   name,email,password:hashedpasswrod
 })
    if(newuser){
-    
-   }
+    await generatetoken(newuser._id,res)
+    await newuser.save()
+    res.status(200).json({message:"created the user"
+    })
+
+   }else{
+      res.status(400).json({message:"user is not created"})
+    }
 
 }catch(e){
-res.status(500).send({message:e.message})
+res.status(500).json({message:e.message})
 }
    
 
 }
 const logout=(req,res)=>{
-  res.send("logout")
+  res.json("logout")
 }
 const login=(req,res)=>{
-  res.send("singup")
+  res.json("singup")
 }
 export  {login,logout,signup}
--- backend/Middlewares/jwt.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken'
-const generatetoken=(userid,res)=>{
-    const token=jwt.sign({userid},process.env.SECRET,{
-        expiresIn='7d'
-    });
-    
-}
