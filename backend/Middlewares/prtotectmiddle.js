f893b2137f40621745c4882e6278141022c08630
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-11 22:11:42 +0530

    testing'

--- backend/Middlewares/prtotectmiddle.js
+++ backend/Middlewares/prtotectmiddle.js
@@ -1,9 +1,19 @@
 import jwt from 'jsonwebtoken'
 import usermode from '../Models/usermodel'
 const protected=async(req,res,next)=>{
+    try{
 const token=req.cookie.jwt
 if(!token)res.status(500).json({message:"no token founded"})
-    const
+    const verify=jwt.verify(token,process.env.SECRET)
+if(!verify)res.status(500).json({message:"not authorizied"})
+    const user=await usermode.findById(token.userid).select("-password")
+if(!user)res.status(500).json({message:"not authorizied"})
+    req.user=user
+next() 
+    }catch(e){
+res.status(500).json({message:e.message})
+    }
+
 
 }
 export default protected
--- backend/Routes/auth.route.js
+++ backend/Routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { login, logout, signup } from '../Controllers/authcr.js'
+import { login, logout, signup ,upload} from '../Controllers/authcr.js'
+import protected from '../Middlewares/prtotectmiddle.js'
 const route = express.Router()
 
 route.post('/login', login)
 route.post('/signup', signup)
 route.post('/logout', logout)
+route.put('/upload',protected,upload)
 
 export default route
