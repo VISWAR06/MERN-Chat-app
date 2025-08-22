fd95cfae54531861e339106baeff3f1568cf082e
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-22 11:18:49 +0530

    msg mode done

--- backend/Models/message.js
+++ backend/Models/message.js
@@ -1,1 +1,16 @@
 import mongoose from "mongoose";
+const msg=mongoose.Schema({
+    senderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        require:true
+    },recvId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        require:true
+    },
+    image:{
+        type:string,
+        deafult:""
+    }
+})
