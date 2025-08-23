302662b43abd4b0cba1c4a6d2760b8bcf10d63f3
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-23 13:45:30 +0530

    route for disp

--- backend/Routes/message.route.js
+++ backend/Routes/message.route.js
@@ -2,3 +2,18 @@ import express from 'express'
 import middle from '../Middlewares/middleware.js'
 const msg=express.Router()
 msg.get('/user',middle,getuser)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default msg
