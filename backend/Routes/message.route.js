0985cc3bf64ccf0429505bff922cac8be8712243
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-23 14:49:49 +0530

    message send

--- backend/Routes/message.route.js
+++ backend/Routes/message.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import middle from '../Middlewares/middleware.js'
-import{getuser,getmessage}  from '../Controllers/messages.js'
+import{getuser,getmessage,sendmessage}  from '../Controllers/messages.js'
 const msg=express.Router()
 msg.get('/user',middle,getuser)
 msg.get('/:id',getmessage)
+msg.post("/send/:id",sendmessage)
 
 
 
