59a2f6bef0e580a2de696c46e4e18c3076180785
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-24 21:00:48 +0530

    all done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Routes } from 'react-router-dom'
+import { Routes ,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Setting from './pages/Setting'
+import Sigin from './pages/Sigin'
+import Profile from './pages/Profile'
+import Signup from './pages/Singup'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Signin/>}/>
+        <Route path='/login' element={<Sigin/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/profile' element={<Profile/>}/>
         <Route path='/setting' element={<Setting/>}/>
