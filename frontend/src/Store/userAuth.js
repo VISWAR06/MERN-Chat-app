0f4ab276fabb3ae2b486d0e4c4a3d29db33c8e71
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-25 14:09:09 +0530

    zustang using checks

--- frontend/src/Store/userAuth.js
+++ frontend/src/Store/userAuth.js
@@ -1,10 +1,23 @@
 import {create} from 'zustand'
+import { axioinstance } from '../library/axios'
 
 const userAuth= create((set)=>({
     user:null,
     isSiginup:false,
     IsSignin:false,
     isUpdating:false,
-    isChecking:true
+    isChecking:true,
+    checkAuth:async()=>{
+        try{
+            const res=await axioinstance.get("/api/check")
+            set({user:res.data})
+        }catch(e){
+            console.log(e.message)
+            set({user:null})
+
+        }finally{
+            set({isChecking:false})
+        }
+    }
 }))
 export default userAuth
