d037f059884e18096070b6c060a1f7004d279bac
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-26 14:57:12 +0530

    search

--- frontend/src/Components/Sidebar comp/Search.jsx
+++ frontend/src/Components/Sidebar comp/Search.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/material/IconButton';
 const Search = () => {
+    const[text,setText]=useState("")
   return (
-    <div>
-      search
+    <div className='bg-white h-20 rounded-2xl mt-4'>
+      <div className='flex items-center h-full'>
+        <IconButton><SearchIcon sx={{fontSize:50}}/></IconButton>
+        <input className='h-16 w-[80%]' placeholder='SEARCH' type='text' value={text}/>
+      </div>
+
     </div>
   )
 }
