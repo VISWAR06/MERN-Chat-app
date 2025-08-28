ae658679fd5584a035cf8a56e5efaaf7bfa6444b
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-08-28 20:05:50 +0530

    noworkk

--- frontend/src/Components/Sidebar comp/Search.jsx
+++ frontend/src/Components/Sidebar comp/Search.jsx
@@ -4,7 +4,7 @@ import IconButton from '@mui/material/IconButton';
 const Search = () => {
     const[text,setText]=useState("")
   return (
-    <div className=' h-20 rounded-2xl mt-4'>
+    <div className=" h-20 rounded-2xl mt-4">
       <div className='flex items-center h-full'>
         <IconButton><SearchIcon sx={{fontSize:50}}/></IconButton>
         <input className='h-16 w-[80%]' placeholder='SEARCH' type='text' value={text}/>
