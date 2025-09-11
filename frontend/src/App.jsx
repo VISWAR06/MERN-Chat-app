06320deb1b11428da099c3c0a9bccfb6a0310ad4
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-09-11 22:41:23 +0530

    pages route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Main from './Components/Main'
+import {Routes,Route}from'react-router-dom'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Profile from './Pages/Profile'
 const App = () => {
   return (
-    <div className=' bg-gray-300 h-screen m-8'>
-      <Main/>
+    <div >
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+      
     </div>
   )
 }
