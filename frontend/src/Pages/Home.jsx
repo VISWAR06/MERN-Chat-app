d789394ad18258147d58585c9fbc2eaa478f0cb1
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-09-12 21:43:04 +0530

    css

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const[user,setUSer]=useState(null);
+
   return (
-    <div>
+    <div className='border w-ful h-screen sm:px-[15%] sm:py-[5%] '>
+      <div className= {'border-2 border-gray-700 rounded-2xl overflow-hidden h-[100%] grid gird-cols-1 relative ${user?"md:grid-cols-[1fr_1.5fr_1fr] xl:frid-cols-[1fr_2fr_1fr]":"md:grid-cols-2"}'}>
+        
+      </div>
       
     </div>
   )
