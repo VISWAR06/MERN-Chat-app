a1c1571ed6f95f5fc7f32d40805e65c01efee01d
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-09-21 20:58:45 +0530

    arcjet

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -4,8 +4,7 @@ import { signup, login, logout, upload } from '../Controllers/User.js';
 import { middleware } from "../Middleware/Auth.js";
 
 const router = express.Router();
-
-router.get('/test', ajprotect, (req, res) => res.status(200).json({ message: "test route" }));
+router.use(ajprotect);
 router.post('/signup', signup);
 router.post('/login', ajprotect, login);
 router.post('/logout', logout);
