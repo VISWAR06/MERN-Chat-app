7b076673fe1c3ebf65c260e2bde2b36be1518fea
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-09-21 22:03:44 +0530

    chages done

--- backend/Controllers/Message.js
+++ backend/Controllers/Message.js
@@ -3,23 +3,26 @@ import messagemode from "../Models/Message.js";
 
 
 export const getchat=async (req,res)=>{
-    try{
-        res.send("working")
-        const reqid = req.user._id
-        const filteruser = await usermodel.find({_id:{$ne:reqid}}).select("-password")
-        res.status(200).json(filteruser)
+   
+    
+}
+export const getmessage=(req,res)=>{
+
+}
+export const getcontact=async (req,res)=>{
+     try{
+     
+const reqid = req.user._id
+const filteruser = await usermodel.find({_id:{$ne:reqid}}).select("-password")
+res.status(200).json(filteruser)
+
+
 
     }catch(e){
         console.log(e.message);
         res.status(500).json({message:e.message})
 
     }
-    
-}
-export const getmessage=(req,res)=>{
-
-}
-export const getcontact=(req,res)=>{
 
 }
 export const sendmessage=(req,res)=>{
--- backend/Middleware/Auth.js
+++ backend/Middleware/Auth.js
@@ -1,19 +1,23 @@
 import usermodel from '../Models/Usermode.js'
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
- const middleware = async(req,res,next)=>{
-    try{
- const token =req.cookies.jwt
-    if(!token)return res.status(400).json({message:"no token"})
-        const decode =  jwt.verify(token,process.env.SECRET)
-    if(!decode)return res.status(400).json({message:"not authorized"})
+
+const middleware = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if (!token) return res.status(400).json({ message: "no token" });
+
+        const decode = jwt.verify(token, process.env.SECRET);
+        if (!decode) return res.status(400).json({ message: "not authorized" });
+
         const user = await usermodel.findById(decode.userid).select('-password');
-    if(!user)return res.status(400).json("not authorized")
-        req.user=user
-        next()
-    }catch(err){
-        res.status(500).json({message:err.message})
+        if (!user) return res.status(400).json("not authorized");
+
+        req.user = user;  // <-- Fix is here
+        next();
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-   
 }
-export default middleware
+
+export default middleware;
