c90acde410f594a99c12cb623a1383eeb94e4a29
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-10-06 20:45:51 +0530

    finish

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,3 +4,7 @@ export const axiosistance = axios.create({
     baseURL:import.meta.env.mode==='development'?'http://localhost:3000/api':'/api',
     withCredentials:true
 })
+new Promise((resolve, reject) => {
+    
+})
+nowork
