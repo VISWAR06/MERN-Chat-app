dfccb18db40899d1588f532dbfb0d1e5e99f140d
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-11-23 20:32:19 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,37 @@
-import React, { useEffect } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import ChatPage from "./Pages/ChatPage";
-import LoginUpPage from "./Pages/LoginUpPage";
-import SignUpPage from "./Pages/SignUpPage";
-import { AuthStore } from "./store/AuthStore.js";
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import PageLoader from "./components/PageLoader";
 
-const App = () => {
-  const { fucheck, user } = AuthStore();
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  const { checkAuth, isCheckingAuth, authUser } = useAuthStore();
 
   useEffect(() => {
-    const fetchAuthData = async () => {
-      try {
-        const data = await fucheck();
-        console.log("✅ Auth Check Response:", data);
-      } catch (error) {
-        console.error("❌ Auth check failed:", error);
-      }
-    }
+    checkAuth();
+  }, [checkAuth]);
 
-    fetchAuthData();
-    // eslint-disable-next-line react-hooks/exhausti
-  }, []);
+  if (isCheckingAuth) return <PageLoader />;
 
   return (
-    <div
-      className="min-h-screen bg-slate-900 relative flex items-center justify-center
-    p-4 overflow-hidden"
-    >
-      <div className="absolute top-0 -left-1 size-96 bg-pink-400 opacity-20 blur-[100px]" />
-      <div className="absolute bottom-0 -right-0 size-96 bg-cyan-400 opacity-20 blur-[100px]" />
-      <div className="absolute bottom-0 -left-0 size-96 bg-cyan-400 opacity-20 blur-[100px]" />
+    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+      {/* DECORATORS - GRID BG & GLOW SHAPES */}
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
       <Routes>
-        <Route
-          path="/"
-          element={user ? <ChatPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/login"
-          element={!user ? <LoginUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/signup"
-          element={!user ? <SignUpPage /> : <Navigate to="/" />}
-        />
-   
+        <Route path="/" element={authUser ? <ChatPage /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+      </Routes>
+
+      <Toaster />
     </div>
   );
-};
-
-export default Ap
+}
+export default App;
